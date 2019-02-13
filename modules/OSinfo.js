37c66185c0dbd7f23209e8762eafd5a18e773f63
Jadwiga Bochinska <matuzalema@gmail.com>
2019-02-13 14:08:14 +0100

    fix time

--- modules/OSinfo.js
+++ modules/OSinfo.js
@@ -1,4 +1,5 @@
 var os = require('os');
+var getTime = require('./time.js');
 
 function getOSinfo() {
     var type = os.type();
@@ -9,14 +10,14 @@ function getOSinfo() {
     }
     var release = os.release();
     var cpu = os.cpus()[0].model;
-    var uptime = os.uptime();
+    var uptime = getTime.print();
     var userInfo = os.userInfo();
-    console.log('System:', type);
-    console.log('Release:', release);
-    console.log('CPU model:', cpu);
-    console.log('Uptime: ~', (uptime / 60).toFixed(0), 'min');
-    console.log('User name:', userInfo.username);
-    console.log('Home dir:', userInfo.homedir);
+    console.log('System: ', type);
+    console.log('Release: ', release);
+    console.log('CPU model: ', cpu);
+    console.log('time: ', uptime);
+    console.log('User name: ', userInfo.username);
+    console.log('Home dir: ', userInfo.homedir);
 }
 
 exports.print = getOSinfo;
